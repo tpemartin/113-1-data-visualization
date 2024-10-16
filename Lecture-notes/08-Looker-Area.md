38ac197f4eea5d3a011f9a1545080360cb4bbc16
Martin老師 <tpemartin@gmail.com>
2024-10-16 12:51:07 +0800

    update

---
+++ Lecture-notes/08-Looker-Area.md
@@ -0,0 +1,6 @@
+# Looker: Area Chart
+
+  - 適合用在呈現細分類別"總和"的變化趨勢，例如：不同產品的銷售量變化趨勢。
+
+## Prepare data
+
--- R/week6-preparation.R
+++ R/week6-preparation.R
@@ -1,8 +1,5 @@
-library(readr)
+library(tidyverse)
 flightDestinationRaw <- read_csv("data/首站抵達地raw.csv")
-
-library(tidyverse)
-library(tidyverse)
 
 # Step 1: Filter the data for "亞洲地區" and convert all year columns to character
 tidy_flight_data <- flightDestinationRaw %>%
@@ -18,8 +15,6 @@ tidy_flight_data <- flightDestinationRaw %>%
 #   "https://docs.google.com/spreadsheets/d/1-jX-3EK_yspYDgPIy5vwnRKHntw9-dQIpFVhLc5JcXc/edit?gid=0#gid=0",
 #   sheet = "flightDestinationAsia"
 # )
-
-library(tidyverse)
 
 # Step 1: Filter the data for "亞洲地區" and convert all year columns to character
 tidy_flight_data <- tidy_flight_data %>%
@@ -41,3 +36,33 @@ ggplot(tidy_flight_data, aes(x = Year, y = Counts, color = 細分)) +
        y = "Counts",
        color = "Category") +
   theme_minimal()
+
+# 移除細分中的"亞洲合計 Total"
+tidy_flight_data <- tidy_flight_data %>%
+  dplyr::filter(細分 != "亞洲合計 Total")
+
+# Parse細分成為factor
+tidy_flight_data <- tidy_flight_data %>%
+  mutate(細分 = factor(細分))
+
+# 計算不同細分的"Counts"總和，並以它的排序來重新排列細分
+tidy_flight_data %>%
+  group_by(細分) %>%
+  summarise(total = sum(Counts, na.rm = T)) %>%
+  arrange(desc(total)) %>% 
+  pull(細分) %>%
+  factor(., levels = .) -> order_levels
+
+# 重新排列細分
+tidy_flight_data <- tidy_flight_data %>%
+  mutate(細分 = factor(細分, levels = order_levels))
+
+# 繪製area堆疊圖。
+ggplot(tidy_flight_data, aes(x = Year, y = Counts, fill = 細分)) +
+  geom_area() +
+  labs(title = "Time Trend of Different Categories in Asia (2002-2022)",
+       x = "Year",
+       y = "Counts",
+       fill = "Category") +
+  theme_minimal()
+
