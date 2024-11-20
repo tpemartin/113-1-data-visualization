64753e7cc9949c5b467edb47493a395ba8f2c5d9
tpemartin <tpemartin@gmail.com>
2024-11-20 17:13:18 +0800

    Update week11-in-class.R

--- R/week11-in-class.R
+++ R/week11-in-class.R
@@ -52,3 +52,62 @@ ggplot() +
     data = tidy_simplified_shp_crop,
     mapping = aes(fill = pop)
   )
+
+# use 
+# 24.94999
+# 121.35977
+# 121.38521
+# 24.93562
+# to create a bounding box for OSM data
+library(osmdata)
+bbox_osm <- c(xmin = 121.35977, ymin = 24.93562, xmax = 121.38521, ymax = 24.94999)
+
+# request OSM data where feature key is "building" and feature value is "university"
+# using open pass query (OPQ) language
+
+osmdata_query <- opq(bbox_osm) %>% 
+     add_osm_feature(key="building", value="university") 
+
+# obtain osmdata using the query
+osmdata <- osmdata_sf(osmdata_query)
+
+osmdata
+
+glimpse(osmdata$osm_polygons)
+
+# ntpu building sf
+ntpu_building_sf <- osmdata$osm_polygons
+glimpse(ntpu_building_sf)
+
+# plot ntpu building sf
+ggplot() +
+  geom_sf(
+    data = ntpu_building_sf
+  )
+
+# new bbox from
+# 
+
+# 25.06316
+# 121.50630
+# 121.60810
+# 25.00569
+
+bbox_osm_mrt <- c(xmin = 121.50630, ymin = 25.00569, xmax = 121.60810, ymax = 25.06316)
+
+# request OSM data where feature key is "railway" and feature value is "subway"
+# using open pass query (OPQ) language
+
+osmdata_query_mrt <- opq(bbox_osm_mrt) %>% 
+     add_osm_feature(key="railway", value="subway")
+
+# obtain osmdata using the query
+osmdata_mrt <- osmdata_sf(osmdata_query_mrt)
+
+tpe_mrt_sf <- osmdata_mrt$osm_lines
+
+# graph
+ggplot() +
+  geom_sf(
+    data = tpe_mrt_sf
+  )
